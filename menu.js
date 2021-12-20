a0f18f7a32316acadae81d6528d19280d7fc4a7a
Gegardus <stgegard@gmail.com>
2021-12-20 18:09:33 +0400

    Add panel to html and css, and js file

---
+++ menu.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
