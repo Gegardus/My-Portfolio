1dad5f4137b2175f2d11feb578a8b0f425640c2a
Gegardus <stgegard@gmail.com>
2021-12-23 14:51:44 +0400

    Fix linter errors

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
 // Mobile
 
-const form = document.querySelector('.form-element');
-const message = 'Email must contain only <br> lower case characters';
+const form = document.querySelector(".form-element");
+const message = "Email must contain only <br> lower case characters";
 
 function showMessage(input, message) {
-  const msg = document.querySelector('.validation-msg');
+  const msg = document.querySelector(".validation-msg");
   msg.innerHTML = `<p class="error-msg">${message}</p>`;
 }
 
@@ -16,8 +16,8 @@ function validateEmail(input) {
   return false;
 }
 
-form.addEventListener('submit', (event) => {
-  const msg = document.querySelector('.validation-msg');
+form.addEventListener("submit", (event) => {
+  const msg = document.querySelector(".validation-msg");
   event.preventDefault();
   const input = form.elements[1];
   const emailValid = validateEmail(input.value);
@@ -32,36 +32,36 @@ form.addEventListener('submit', (event) => {
 
 // Desktop
 
-const mediaQuery = window.matchMedia( '( min-width: 992px )' );
+const mediaQuery = window.matchMedia("( min-width: 992px )");
 
-if (mediaQuery.matches) { 
-const form = document.querySelector('.formDesk-element');
-const message = 'Email must contain only <br> lower case characters';
+if (mediaQuery.matches) {
+  const form = document.querySelector(".formDesk-element");
+  const message = "Email must contain only <br> lower case characters";
 
-function showMessage(input, message) {
-  const msg = document.querySelector('.validationDesk-msg');
-  msg.innerHTML = `<p class="error-msg">${message}</p>`;
-}
-
-function validateEmail(input) {
-  const value = input.toLowerCase();
-  if (value.localeCompare(input) === 0) {
-    return true;
+  function showMessage(input, message) {
+    const msg = document.querySelector(".validationDesk-msg");
+    msg.innerHTML = `<p class="error-msg">${message}</p>`;
   }
-  return false;
-}
 
-form.addEventListener('submit', (event) => {
-  const msg = document.querySelector('.validationDesk-msg');
-  event.preventDefault();
-  const input = form.elements[1];
-  const emailValid = validateEmail(input.value);
-  if (emailValid) {
-    msg.remove();
-    form.submit();
-    form.reset();
-  } else {
-    showMessage(input, message, false);
+  function validateEmail(input) {
+    const value = input.toLowerCase();
+    if (value.localeCompare(input) === 0) {
+      return true;
+    }
+    return false;
   }
-});
+
+  form.addEventListener("submit", (event) => {
+    const msg = document.querySelector(".validationDesk-msg");
+    event.preventDefault();
+    const input = form.elements[1];
+    const emailValid = validateEmail(input.value);
+    if (emailValid) {
+      msg.remove();
+      form.submit();
+      form.reset();
+    } else {
+      showMessage(input, message, false);
+    }
+  });
 }
