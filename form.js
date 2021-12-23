2f8f3364ce12aca2b164a5ab2ee96c80244b33bd
Gegardus <stgegard@gmail.com>
2021-12-23 19:33:00 +0400

    Fix linter errors

--- form.js
+++ form.js
@@ -36,22 +36,22 @@ form.addEventListener('submit', (event) => {
 
 const mediaQuery = window.matchMedia('( min-width: 992px )');
 
+function showMessage(input, message) {
+  const msg = document.querySelector('.validationDesk-msg');
+  msg.innerHTML = `<p class="error-msg">${message}</p>`;
+}
+
+function validateEmail(input) {
+  const value = input.toLowerCase();
+  if (value.localeCompare(input) === 0) {
+    return true;
+  }
+  return false;
+}
+
 if (mediaQuery.matches) {
   const form = document.querySelector('.formDesk-element');
   const message = 'Email must contain only <br> lower case characters';
-
-  function showMessage(input, message) {
-    const msg = document.querySelector('.validationDesk-msg');
-    msg.innerHTML = `<p class="error-msg">${message}</p>`;
-  }
-
-  function validateEmail(input) {
-    const value = input.toLowerCase();
-    if (value.localeCompare(input) === 0) {
-      return true;
-    }
-    return false;
-  }
 
   form.addEventListener('submit', (event) => {
     const msg = document.querySelector('.validationDesk-msg');
