98eb18e357ca17056ce3b499c97ecd9ac21403f1
Gegardus <stgegard@gmail.com>
2021-12-23 10:50:08 +0400

    Add media query

--- form.js
+++ form.js
@@ -27,3 +27,40 @@ form.addEventListener('submit', (event) => {
     showMessage(input, message, false);
   }
 });
+
+// Desktop
+
+const mediaQuery = window.matchMedia( '( min-width: 992px )' )
+
+if ( mediaQuery.matches ) {
+ 
+const form = document.querySelector('.formDesk-element');
+const message = 'Email must contain only <br> lower case characters';
+
+function showMessage(input, message) {
+  const msg = document.querySelector('.validationDesk-msg');
+  msg.innerHTML = `<p class="error-msg">${message}</p>`;
+}
+
+function validateEmail(input) {
+  const value = input.toLowerCase();
+  if (value.localeCompare(input) === 0) {
+    return true;
+  }
+  return false;
+}
+
+form.addEventListener('submit', (event) => {
+  const msg = document.querySelector('.validationDesk-msg');
+  event.preventDefault();
+  const input = form.elements[1];
+  const emailValid = validateEmail(input.value);
+  if (emailValid) {
+    msg.remove();
+    form.submit();
+    form.reset();
+  } else {
+    showMessage(input, message, false);
+  }
+});
+}
