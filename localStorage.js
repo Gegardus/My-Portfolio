bde02c3ab1e0a0d1cd1acb46c90f1c7b30a9cbea
Gegardus <stgegard@gmail.com>
2021-12-23 23:05:32 +0400

    Add media query

--- localStorage.js
+++ localStorage.js
@@ -1,6 +1,6 @@
-const name1 = document.querySelector('#name');
-const email1 = document.querySelector('#mail');
-const message1 = document.querySelector('#msg');
+const name1 = document.querySelector('.name1');
+const email1 = document.querySelector('.email1');
+const message1 = document.querySelector('.msg1');
 
 const storeValues = (name1, email1, message1) => {
   const dataObject = JSON.stringify({ name1, email1, message1 });
@@ -24,3 +24,36 @@ function getValues() {
 
 populateStorage();
 getValues();
+
+// Desktop
+
+const mediaQuery = window.matchMedia( '( min-width: 992px )' );
+
+if ( mediaQuery.matches ) {
+const name2 = document.querySelector('#my-name');
+const email2 = document.querySelector('#my-mail');
+const message2 = document.querySelector('#my-msg');
+
+const storeDeskValues = (name2, email2, message2) => {
+  const dataObject = JSON.stringify({ name2, email2, message2 });
+  localStorage.setItem('dataObject', dataObject);
+};
+
+function populateDeskStorage() {
+  const formDeskValues = JSON.parse(localStorage.getItem('dataObject'));
+  if (formDeskValues) {
+    name2.value = formDeskValues.name2;
+    email2.value = formDeskValues.email2;
+    message2.value = formDeskValues.message2;
+  }
+}
+
+function getDeskValues() {
+  name1.addEventListener('input', () => storeDeskValues(name2.value, email2.value, message2.value));
+  email1.addEventListener('input', () => storeDeskValues(name2.value, email2.value, message2.value));
+  message1.addEventListener('input', () => storeDeskValues(name2.value, email2.value, message2.value));
+}
+}
+
+populateDeskStorage();
+getDeskValues();
