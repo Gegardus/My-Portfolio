1fed115ae41b476a40d8202ce902f789261a556b
Gegardus <stgegard@gmail.com>
2021-12-24 14:37:32 +0400

    Update popup js file

---
+++ popup.js
@@ -0,0 +1,99 @@
+
+const projects = [
+    {
+      title: "Multi-Post Stories",
+      description:
+        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+      techs: ["css", "html", "Bootstrap", "Ruby"],
+      featuredImage: "assets/images/multi-post.svg",
+      featuredPopupImage: "assets/images/multi-post.svg",
+      liveVersionon: "https://gegardus.github.io/My-Portfolio/",
+      sourceLink: "https://github.com/Gegardus/Desktop-Version/tree/desktop",
+      class: "background",
+      btnImg1: 'assets/images/seelive.svg',
+      btnImg2: 'assets/images/seeSrc.png',
+    },
+    {
+      title: "Profesional Art Printing Data",
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
+      techs: ["html", "Bootstrap", "Ruby"],
+      featuredImage: "assets/images/ImgI.svg",
+      featuredPopupImage: "assets/images/popDesk.png",
+      liveVersion: "https://gegardus.github.io/My-Portfolio/",
+      sourceLink: "https://github.com/Gegardus/Desktop-Version/tree/desktop",
+      class: "work1",
+      btnImg1: 'assets/images/seelive.svg',
+      btnImg2: 'assets/images/seeSrc.png',
+    },
+    {
+      title: "Data Dashboard Healthcare",
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
+      techs: ["html", "Bootstrap", "Ruby"],
+      featuredImage: "assets/images/ImgII.svg",
+      featuredPopupImage: "assets/images/popDesk.png",
+      liveVersion: "https://gegardus.github.io/My-Portfolio/",
+      sourceLink: "https://github.com/Gegardus/Desktop-Version/tree/desktop",
+      class: "work2",
+      btnImg1: 'assets/images/seelive.svg',
+      btnImg2: 'assets/images/seeSrc.png',
+    },
+    {
+      title: "Website Protfolio",
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
+      techs: ["html", "Bootstrap", "Ruby"],
+      featuredImage: "assets/images/ImgIII.svg",
+      featuredPopupImage: "assets/images/popDesk.png",
+      liveVersion: "https://gegardus.github.io/My-Portfolio/",
+      sourceLink: "https://github.com/Gegardus/Desktop-Version/tree/desktop",
+      class: "work3",
+      btnImg1: 'assets/images/seelive.svg',
+      btnImg2: 'assets/images/seeSrc.png',
+    },
+    {
+      title: "Profesional Art Printing Data",
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
+      techs: ["html", "Bootstrap", "Ruby"],
+      featuredImage: "assets/images/ImgIV.svg",
+      featuredPopupImage: "assets/images/popDesk.png",
+      liveVersion: "https://gegardus.github.io/My-Portfolio/",
+      sourceLink: "https://github.com/Gegardus/Desktop-Version/tree/desktop",
+      class: "work4",
+      btnImg1: 'images/btn-img-pu1.png',
+      btnImg1: 'assets/images/seelive.svg',
+      btnImg2: 'assets/images/seeSrc.png',
+    },
+    {
+      title: "Data Dashboard Healthcare",
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
+      techs: ["html", "Bootstrap", "Ruby"],
+      featuredImage: "assets/images/ImgII.svg",
+      featuredPopupImage: "assets/images/popDesk.png",
+      liveVersion: "https://gegardus.github.io/My-Portfolio/",
+      sourceLink: "https://github.com/Gegardus/Desktop-Version/tree/desktop",
+      class: "work5",
+      btnImg1: 'assets/images/seelive.svg',
+      btnImg2: 'assets/images/seeSrc.png',
+    },
+    {
+      title: "Website Protfolio",
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry"s standard.',
+      techs: ["html", "Bootstrap", "Ruby"],
+      featuredImage: "assets/images/ImgIII.svg",
+      featuredPopupImage: "assets/images/popDesk.png",
+      liveVersion: "https://gegardus.github.io/My-Portfolio/",
+      sourceLink: "https://github.com/Gegardus/Desktop-Version/tree/desktop",
+      className: "work6",
+      btnImg1: 'assets/images/seelive.svg',
+      btnImg2: 'assets/images/seeSrc.png',
+    },
+  ];
+  
+  const buttonOne = document.querySelector(".bn-button");
+  const buttonTwo = document.querySelector(".pr-button");
+  
