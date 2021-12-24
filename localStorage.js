509e7c3d0593dca4a6f5bc073b5de24337b69f8a
Gegardus <stgegard@gmail.com>
2021-12-24 12:05:50 +0400

    Update localStorage file

--- localStorage.js
+++ localStorage.js
@@ -1,6 +1,6 @@
-const name1 = document.querySelector('.name1', '#my-name');
-const email1 = document.querySelector('.email1', '#my-mail');
-const message1 = document.querySelector('.msg1', '#my-msg');
+const name1 = document.querySelector('.name1');
+const email1 = document.querySelector('.email1');
+const message1 = document.querySelector('.msg1');
 
 const storeValues = (name1, email1, message1) => {
   const dataObject = JSON.stringify({ name1, email1, message1 });
@@ -25,35 +25,30 @@ function getValues() {
 populateStorage();
 getValues();
 
-const mediaQuery = window.matchMedia( '( min-width: 992px )' );
+const name2 = document.querySelector('#my-name');
+  const email2 = document.querySelector('#my-mail');
+  const message2 = document.querySelector('#my-msg');
 
-if ( mediaQuery.matches ) {
-  const name1 = document.querySelector('.name1');
-  const email1 = document.querySelector('.email1');
-  const message1 = document.querySelector('.msg1');
-  
-  const storeValues = (name1, email1, message1) => {
-    const dataObject = JSON.stringify({ name1, email1, message1 });
-    localStorage.setItem('dataObject', dataObject);
-  };
-  
-  function populateStorage() {
-    const formValues = JSON.parse(localStorage.getItem('dataObject'));
-    if (formValues) {
-      name1.value = formValues.name1;
-      email1.value = formValues.email1;
-      message1.value = formValues.message1;
-    }
+const storeValues2 = (name2, email2, message2) => {
+  const dataObject2 = JSON.stringify({ name2, email2, message2 });
+  localStorage.setItem('dataObject2', dataObject2);
+};
+
+
+function populateStorage2() {
+  const formValues2 = JSON.parse(localStorage.getItem('dataObject2'));
+  if (formValues2) {
+    name2.value = formValues2.name2;
+    email2.value = formValues2.email2;
+    message2.value = formValues2.message2;
   }
-  
-  function getValues() {
-    name1.addEventListener('input', () => storeValues(name1.value, email1.value, message1.value));
-    email1.addEventListener('input', () => storeValues(name1.value, email1.value, message1.value));
-    message1.addEventListener('input', () => storeValues(name1.value, email1.value, message1.value));
-  }
-
-  populateStorage();
-  getValues();
-
 }
 
+function getValues2() {
+  name2.addEventListener('input', () => storeValues2(name2.value, email2.value, message2.value));
+  email2.addEventListener('input', () => storeValues2(name2.value, email2.value, message2.value));
+  message2.addEventListener('input', () => storeValues2(name2.value, email2.value, message2.value));
+}
+
+populateStorage2();
+getValues2();
